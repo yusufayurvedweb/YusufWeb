8df7305be0d06ce431e661db142451b4af1ce2c0
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-20 17:33:07 +0530

    Create server.js

---
+++ backend-server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.use(cors()); // 🛑 This must come BEFORE your routes
+
+app.use(express.json());
+const productsRoutes = require('./routes/products');
+app.use('/api/products', productsRoutes);
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
