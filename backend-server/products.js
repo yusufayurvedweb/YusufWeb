0442399764856236ed5bdd807eb29be2f0f0bbbf
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-20 22:07:49 +0530

    Update products.js

--- backend-server/products.js
+++ backend-server/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/database');
+const db = require('./database');
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
