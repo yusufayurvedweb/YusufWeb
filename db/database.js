9bed3e64751388158aecad9ea384dc06b96db994
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-21 06:49:25 +0530

    Update database.js

--- db/database.js
+++ db/database.js
@@ -2,9 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'mysql-22404967-yusufayurvedwebsite.j.aivencloud.com',
+  port : 11857,
   user: 'avnadmin',
   password: 'AVNS_UJXfbFkb9e7s50oTzkl',
-  database: ''
+  database: 'defaultdb'
 });
 
 module.exports = pool.promise();
