4d3d2da9610d0f2e2a1edbb43e3d172d69f80201
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-21 15:12:30 +0530

    defpage.js

--- defpage.js
+++ defpage.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   function displayProductDetails(product) {
     const container = document.getElementById('carousel');
     container.innerHTML = `
-      
+      <div>
         <img src="${product.image}" alt="${product.name}" class="carousel-image" width=200/>
         <div class="desc">
           <h2>${product.name}</h2>
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', async () => {
           <p class="description">${product.description || "No description available."}</p>
           <button class="shop-now-btn">Buy Now</button>
       
-      </div>
+      </div></div>
     `;
   }
 
