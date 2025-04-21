14a6b26133f14d714e22680dc345412b30f06101
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-21 14:04:38 +0530

    Update defpage.js

--- defpage.js
+++ defpage.js
@@ -26,15 +26,15 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
   
   function displayProductDetails(product) {
-    const container = document.getElementById('carousel-scroll');
+    const container = document.getElementById('carousel');
     container.innerHTML = `
       <div >
         <img src="${product.image}" alt="${product.name}" class="carousel-image" width=200/>
-        <div class="carousel-item">
+        <div class="product-grid">
           <h2>${product.name}</h2>
           <p class="price">Price: ₹${product.price}</p>
           <p class="description">${product.description || "No description available."}</p>
-          <button class="buy-now">Buy Now</button>
+          <button class="shop-now-btn">Buy Now</button>
         </div>
       </div>
     `;
