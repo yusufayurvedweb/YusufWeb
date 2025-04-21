87bc75a680464f78a10338ffe4a903e90ac52d12
yusufayurvedweb <yusufayurvedwebsite@gmail.com>
2025-04-21 06:27:57 +0530

    Update database.js

--- db/database.js
+++ db/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'mysql-22404967-yusufayurvedwebsite.j.aivencloud.com',
-  user: 'AbhiAndSanjeevani',
-  password: 'abhiandsanjeevani',
-  database: 'ecomfash'
+  user: 'avnadmin',
+  password: 'AVNS_UJXfbFkb9e7s50oTzkl',
+  database: ''
 });
 
 module.exports = pool.promise();
